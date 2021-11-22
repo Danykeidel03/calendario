3aac66e0e487688937977474879c2cc51792f35c
Daniel <supertuxcar@gmail.com>
2021-11-22 13:41:54 +0100

    fechas1

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -3,39 +3,51 @@ public class CalendarioBasico {
     private int dia;
     private int mes;
     private int ano;
-   
+
     public CalendarioBasico () {
         dia = 1;
         mes = 1;
         ano = 1;
-        
     }
+
     public String obtenerFecha() {
-     return (dia + "-" + mes +"-" + ano );
+        String newDia = "";
+        String newMes = "";
+        String newAno = "";
+        if (dia < 10) {
+            newDia = "0" + dia;
+        }
+         if (mes < 10) {
+            newMes = "0" + mes;
+        }
+         if (ano < 10) {
+            newAno = "0" + ano;
+        }
+        return (newDia + "-" + newMes +"-" + newAno );
     }
+
     public void fijarFecha(int nuevoDia, int nuevoMes, int nuevoAno) {
-    dia = nuevoDia;
-    mes = nuevoMes;
-    ano = nuevoAno;
+        dia = nuevoDia;
+        mes = nuevoMes;
+        ano = nuevoAno;
     }
-    
+
     public void avanzarFecha() {
-    dia = dia + 1;
-    if (dia > 30) {
-    dia = 1;
-    mes = mes + 1;
-    }
-    if (dia > 12) {
-    dia = 1;
-    mes = 1;
-    ano = ano + 1;
-    }
-    if (ano > 99)
-    dia = 1;
-    mes = 1;
-    ano = 1;
-    
-    }
-    
-    
-} 
+        dia = dia + 1;
+        if (dia > 30) {
+            dia = 1;
+            mes = mes + 1;
+        }
+        if (mes > 12) {
+            dia = 1;
+            mes = 1;
+            ano = ano + 1;
+        }
+        if (ano > 99) {
+            dia = 1;
+            mes = 1;
+            ano = 1;
+        }
+
+    } 
+}
